d8293521c44a7365473a8586aea8ab212a4f6662
jpaligorov <jpaligorov@icloud.com>
2021-06-09 17:40:16 +1000

    Mid Section 10 updates

--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using AutoMapper;
 using MediatR;
 using Persistence;
@@ -9,12 +10,12 @@ namespace Application.Activities
 {
     public class Delete
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -22,15 +23,20 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
-                var activity = await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities.FindAsync(new object[]{request.Id}, cancellationToken: cancellationToken);
+
+                // if (activity == null)
+                //     return null;
 
                 _context.Remove(activity);
                 
-                await _context.SaveChangesAsync(cancellationToken);
+                var result = await _context.SaveChangesAsync(cancellationToken) > 0;
 
-                return Unit.Value;
+                if (!result) return Result<Unit>.Failure("Failed to delete activity");
+
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
